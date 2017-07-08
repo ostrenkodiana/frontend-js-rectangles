e1cbd03d382953044bc93aeb62a25012f46c6a29
Diana Ostrenko <ostrenkodiana1@gmail.com>
2017-07-08 13:08:13 +0300

    finish both of functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-﻿
+﻿'use strict';
+
+function areIntersected(a, b) {
+    if (a.top >= (b.top + b.height) ||
+        (a.top + a.height) <= b.top ||
+        a.left >= (b.left + b.width) ||
+        (a.left + a.width) <= b.left) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+
+function filterVisible(a, array) {
+    return array.filter(function (element) {
+        if (element.width !== 0 && element.height !== 0) {
+            return areIntersected(a, element);
+        } else {
+            return false;
+        }
+    });
+};
