89509471e40bc4e7b5dd11af3e54397d8bdb8ccb
Diana Ostrenko <ostrenkodiana1@gmail.com>
2017-07-10 12:25:08 +0300

    delete useless code

--- src/index.js
+++ src/index.js
@@ -1,36 +1,26 @@
 ﻿'use strict';
 
-let rectangleA = {
-    top: (Integer),
-    left: (Integer),
-    height: (Integer),
-    width: (Integer)
-}
-let rectangleB = {
-    top: (Integer),
-    left: (Integer),
-    height: (Integer),
-    width: (Integer)
-}
+let rectangleA;
+let rectangleB;
+
 
 function areIntersected(rectangleA, rectangleB) {
-    if (rectangleA.top >= (rectangleB.top + rectangleB.height) ||
-        (rectangleA.top + rectangleA.height) <= rectangleB.top ||
-        rectangleA.left >= (rectangleB.left + rectangleB.width) ||
-        (rectangleA.left + rectangleA.width) <= rectangleB.left) {
-        return false;
-    } else {
-        return true;
-    }
-};
+	if (rectangleA.top >= (rectangleB.top + rectangleB.height) ||
+		(rectangleA.top + rectangleA.height) <= rectangleB.top ||
+		rectangleA.left >= (rectangleB.left + rectangleB.width) ||
+		(rectangleA.left + rectangleA.width) <= rectangleB.left) {
+		return false;
+	} else {
+		return true;
+	}
+}
 
-
-function filterVisible(a, array) {
-    return array.filter(function (element) {
-        if (element.width !== 0 && element.height !== 0) {
-            return areIntersected(a, element);
-        } else {
-            return false;
-        }
-    });
-};
+function filterVisible(rectangleA, array) {
+	return array.filter(function (element) {
+		if (element.width !== 0 && element.height !== 0) {
+			return areIntersected(a, element);
+		} else {
+			return false;
+		}
+	});
+}
